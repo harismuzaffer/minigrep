ab6f3432f1c22ed4a8dfb8b8c1cef4bf01188462
haris <harismuzaffer@gmail.com>
2022-04-21 23:14:01 +0530

    basic version - no functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let config = parse_config(&args);
+
+    println!("Searching for {} in file {}", config.query, config.filename);
+
+    let contents = fs::read_to_string(config.filename).expect("ohhh");
+    println!("contents are {}", contents);
+}
+
+#[derive(Debug)]
+struct Config<'a> {
+    query: &'a String,
+    filename: &'a String
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = &args[1];
+    let filename = &args[2];
+    
+    Config {
+        query,
+        filename
+    }
 }
