ef2e3779639d036f2b5a0a6a1f1056bf1f9b3329
haris <harsimuzaffer@gmail.com>
2022-05-25 07:48:26 +0530

    feat: add test fn, search fn and call search from run

--- src/lib.rs
+++ src/lib.rs
@@ -26,8 +26,40 @@ impl<'a> Config<'a> {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
+
+    for l in search(&config.query, &contents) {
+        println!("{}", l);
+    }
 
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut v = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            v.push(line);
+        }
+    }
+
+    v
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "rom";
+        let contents = "\
+writing in Rust:
+working from home
+and enjoying the coffee.
+";
+
+        assert_eq!(vec!["working from home"], search(query, contents));
+    }
+}
